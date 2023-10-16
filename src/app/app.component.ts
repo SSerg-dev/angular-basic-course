4576a6079413d66fc75feeefa8e60453f38dee32
SSerg-dev <srmail@mail.ru>
2023-10-16 11:22:55 +0300

    lesson #15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Initial';
-  toggle = false
+  items = [1, 1, 2, 3, 5, 8, 13];
+  objects = [
+    {
+      title: 'post #1',
+      author: 'Sergei',
+      comments: [
+        { name: 'Max', text: 'lorem 1' },
+        { name: 'Inna', text: 'lorem 2' },
+        { name: 'Evgeniia', text: 'lorem 3' },
+      ],
+    },
+    {
+      title: 'post #2',
+      author: 'Sergei 2',
+      comments: [
+        { name: 'Max 2', text: 'lorem 1' },
+        { name: 'Inna 2', text: 'lorem 2' },
+        { name: 'Evgeniia 2', text: 'lorem 3' },
+      ],
+    },
+  ];
 
   constructor() {}
 
   // methods
-
-
 }
-
-
