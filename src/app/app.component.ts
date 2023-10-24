0f255633a5b2c21316af1d143d84033df53b17a7
SSerg-dev <srmail@mail.ru>
2023-10-24 14:07:37 +0300

    lesson #5.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,41 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  date: Date = new Date()
-  private intervalDate: any = null
+  protected date: Date = new Date();
+  private intervalDate: unknown = null;
+
+  search = '';
+  searchField = 'title';
+
+  posts: Post[] = [
+    { title: 'beer', text: 'the best beer' },
+    { title: 'beer light', text: 'the best beer' },
+    { title: 'vodke', text: 'the best vodka' },
+    { title: 'rom', text: 'text best rom' },
+    { title: 'tekilla', text: 'text best tekilla' },
+    { title: 'coniac', text: 'text best coniac' },
+  ];
 
   constructor() {}
+
   // methods
   ngOnInit(): void {
-   this.intervalDate = setInterval(() => {
-      this.date = new Date()
-    }, 1000)
+    this.intervalDate = setInterval(() => {
+      this.date = new Date();
+    }, 1000);
   }
-
   ngOnDestroy(): void {
-      console.log("🚀 ~ file: app.component.ts:21 ~ AppComponent ~ ngOnDestroy ~ ngOnDestroy:")
-      this.intervalDate = null
-
+    this.intervalDate = null;
   }
-
-
-
 }
---
+++ src/app/pipes/filter.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Post } from '../app.component';
+
+@Pipe({
+  name: 'filter',
+})
+export class FilterPipe implements PipeTransform {
+  transform(
+    posts: Post[],
+    search = '',
+    field= 'title'
+  ): Post[] {
+    if (!search.trim()) {
+      return posts;
+    } else {
+      return posts.filter((post: any) => {
+        return post[field]
+          .toLocaleUpperCase()
+          .includes(search.toLocaleUpperCase());
+
+      });
+    }
+    // else {
+    //   return posts.filter((post) => post.title === search)
+    // }
+  }
+}
