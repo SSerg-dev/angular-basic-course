d14d6bf4a073b0d4846fc248efd59fac15d8ccf6
SSerg-dev <srmail@mail.ru>
2023-10-26 11:05:37 +0300

    Rx.JS lesson# 17.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,50 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, Subscription, interval } from 'rxjs';
-import { isSubscription } from 'rxjs/internal/Subscription';
-import { map, filter } from 'rxjs/operators';
+import {Component} from '@angular/core'
+import {Subscription, Observable} from 'rxjs'
+import set = Reflect.set
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  subscription!: Subscription;
-  isSubscription: boolean = false;
-  interval$: Observable<number>;
-  title = 'Stop interval';
+export class AppComponent {
+
+  sub: Subscription
 
   constructor() {
-    this.interval$ = interval(1000);
-    this.subscribe();
+    const stream$ = new Observable(observer => {
+
+      setTimeout(() => {
+        observer.next(1)
+      }, 1500)
+
+
+      setTimeout(() => {
+        observer.complete()
+      }, 2100)
+
+      setTimeout(() => {
+        observer.error('Something went wrong')
+      }, 2000)
+
+
+      setTimeout(() => {
+        observer.next(2)
+      }, 2500)
+
+    })
+
+    this.sub = stream$
+      .subscribe(
+        value => console.log('Next: ', value),
+        error => console.log('Error: ', error),
+        () => console.log('Complete')
+      )
   }
 
-  // methods
-  subscribe() {
-    this.subscription = this.interval$
-      .pipe(
-        filter((value) => value % 2 === 0),
-        map((value) => `mapped value ${value}`)
-      )
-      .subscribe((value) => {
-        console.log('start subscribe:', value);
-      });
-  }
 
   stop() {
-    this.isSubscription = !this.isSubscription;
-    if (this.isSubscription) {
-      this.subscription.unsubscribe();
-      this.title = 'Start interval';
-    } else {
-      this.subscribe();
-      this.title = 'Stop interval';
-    }
+    this.sub.unsubscribe()
   }
-
-  ngOnInit(): void {}
-  ngOnDestroy(): void {}
 }
+
