fbb71dabd2d1a42bb5cc26db4537721f0b35c043
SSerg-dev <srmail@mail.ru>
2023-11-01 16:28:53 +0300

    lesson #7.13 create owner form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { libValidator } from './lib.validator';
 
 @Component({
   selector: 'app-root',
@@ -8,66 +6,15 @@ import { libValidator } from './lib.validator';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  form!: FormGroup;
+
+  appState = 'off'
   constructor() {}
 
   // methods
-  get skills() {
-    return this.form.controls['skills'] as FormArray;
+  handlerChange() {
+    console.log("🚀 ~ file: app.component.ts:16 ~ AppComponent ~ handlerChange ~ this.appState:", this.appState)
   }
 
-  addSkill() {
-    const control = new FormControl('', Validators.required);
-    // (<FormArray>this.form.get('skills')).push(control)
-    (this.form.get('skills') as FormArray).push(control);
-  }
-  setCapital() {
-    const cityMap = {
-      ru: 'Moscow',
-      ua: 'Kyev',
-      by: 'Minsk',
-    };
-
-    const key = this.form.get('address')?.get('country')?.value;
-    const city = cityMap[key as keyof typeof cityMap];
-
-    this.form.patchValue({
-      address: { city },
-    });
-  }
-  submit() {
-    if (this.form.valid) {
-      const formData = { ...this.form.value };
-      console.log(
-        '🚀 ~ file: app.component.ts:17 ~ AppComponent ~ submit ~ formData:',
-        formData
-      );
-      // add clear form
-      this.form.reset();
-    }
-  }
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      email: new FormControl(
-        '',
-        [Validators.email, Validators.required, libValidator.restrictedEmails],
-        [libValidator.uniqEmail as any]
-      ),
-
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(6),
-      ]),
-
-      address: new FormGroup({
-        country: new FormControl('ru'),
-        city: new FormControl('Moscow', Validators.required),
-      }),
-
-      skills: new FormArray([]),
-    });
-
-    // libValidator.start();
-  }
+  ngOnInit(): void {}
   ngOnDestroy(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SwitchComponent],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [
-    // AppCounterService
   ],
   bootstrap: [AppComponent],
 })
