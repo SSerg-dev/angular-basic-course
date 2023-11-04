8312e4b012757d53d5170979403fe089dc351a23
SSerg-dev <srmail@mail.ru>
2023-11-04 14:11:32 +0300

    create chapter #8  lesson #8.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, OnDestroy } from '@angular/core';
+
+export interface Todo {
+  completed: boolean;
+  title: string;
+  id?: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,15 +13,41 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
+  todos: Todo[] = [];
 
-  appState = 'off'
-  constructor() {}
+  todoTitle = '';
+  url = 'https://jsonplaceholder.typicode.com/todos';
+  body = {};
+
+  constructor(private http: HttpClient) {}
 
   // methods
-  handlerChange() {
-    console.log("🚀 ~ file: app.component.ts:16 ~ AppComponent ~ handlerChange ~ this.appState:", this.appState)
-  }
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return;
+    }
+    const newTodo = {
+      title: this.todoTitle,
+      complited: false,
+    };
 
-  ngOnInit(): void {}
+    this.body = newTodo
+    this.http.post<Todo>(this.url, this.body)
+      .subscribe(todo => {
+        console.log("🚀 ~ file: app.component.ts:37 ~ AppComponent ~ todo:", todo)
+        this.todos.unshift(todo)
+        this.todoTitle = ''
+
+      })
+  }
+  ngOnInit(): void {
+    this.http
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe((todos) => {
+        this.todos = todos;
+
+
+      });
+  }
   ngOnDestroy(): void {}
 }
