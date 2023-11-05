8773a5795509ebe9026ecb2c3c6cc19e4cdbc022
SSerg-dev <srmail@mail.ru>
2023-11-05 17:46:54 +0300

    lesson #8.8 catchError

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,26 +13,31 @@ export class AppComponent implements OnInit, OnDestroy {
   loading = false;
 
   todoTitle = '';
+  error = ''
+
   url = 'https://jsonplaceholder.typicode.com/todos';
   body = {};
 
   constructor(private todosService: TodosService) {}
 
   // methods
-  removeTodo(id: number | undefined) {
-    this.todosService.removeTodo(id)
+  completeTodo(id: number | undefined) {
+    if (!id) {
+      return;
+    }
+    this.todosService.completeTodo(id)
     .subscribe((response) => {
+      this.todos.find((todo) => todo.id === id)!.completed = true
+    })
+  }
+  removeTodo(id: number | undefined) {
+    if (!id) {
+      return;
+    }
+
+    this.todosService.removeTodo(id).subscribe((response) => {
       this.todos = this.todos.filter((todo) => todo.id !== id);
     });
-  }
-
-  fetchTodos() {
-    this.loading = true;
-    this.todosService.fetchTodos()
-      .subscribe((todos) => {
-        this.todos = todos;
-        this.loading = false;
-      });
   }
   addTodo() {
     if (!this.todoTitle.trim()) {
@@ -41,16 +46,28 @@ export class AppComponent implements OnInit, OnDestroy {
 
     const newTodo: Todo = {
       title: this.todoTitle,
-      completed: false
+      completed: false,
     };
-    console.log("🚀 ~ file: app.component.ts:46 ~ AppComponent ~ newTodo:", newTodo.title)
 
-    this.todosService.addTodo(newTodo)
-    .subscribe((todo) => {
+    this.todosService.addTodo(newTodo).subscribe((todo) => {
       this.todos.unshift(todo);
       this.todoTitle = '';
     });
-
+  }
+  fetchTodos() {
+    this.loading = true;
+    this.todosService.fetchTodos().subscribe((todos) => {
+      this.todos = todos;
+      this.loading = false;
+    },
+    error => {
+      console.log('error:', error.message)
+      this.error = error.message
+    },
+    // () => {
+    //   console.log('complete')
+    // }
+    );
   }
   ngOnInit(): void {
     this.fetchTodos();
--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, delay } from 'rxjs';
+import { Observable, catchError, delay, throwError } from 'rxjs';
 
 export interface Todo {
   completed: boolean;
@@ -16,16 +16,30 @@ export class TodosService {
   constructor(private http: HttpClient) {}
 
   // methods
+  completeTodo(id: number): Observable<Object> {
+    return this.http.put<Todo>(`${this.url}/${id}`, {
+      completed: true
+    })
+  }
+  removeTodo(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`)
+  }
   addTodo(todo: Todo): Observable<Todo> {
     return this.http.post<Todo>(this.url, todo)
   }
   fetchTodos(): Observable<Todo[]> {
-     return this.http.get<Todo[]>(
-      'https://jsonplaceholder.typicode.com/todos?_limit=2'
+    return this.http.get<Todo[]>(
+     'https://jsonplaceholder.typicode.com/todos?_limit=2'
+   )
+   .pipe(
+    delay(1000),
+    // catchError((error) => {
+    //   console.log("🚀 ~ catchError ~ error:", error)
+    //   return throwError(error)
+
+    // })
     )
-    .pipe(delay(1000))
-  }
-  removeTodo(id: number | undefined): Observable<void> {
-    return this.http.delete<void>(`${this.url}/${id}`)
-  }
+
+ }
+
 }
