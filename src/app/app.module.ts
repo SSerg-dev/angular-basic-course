09077a0c1dde6582ecb85024b2c26bfa90c921e2
SSerg-dev <srmail@mail.ru>
2023-11-10 14:27:54 +0300

    lesson #10.4 modules lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser'
-import {NgModule} from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms'
-import {AboutComponent} from './about/about.component'
-import {HomeComponent} from './home/home.component'
-import {PostsComponent} from './posts/posts.component'
-import {PostComponent} from './post/post.component'
-import {AboutExtraComponent} from './about-extra/about-extra.component'
-import {AppRoutingModule} from './app-routing.module';
-import { ErrorPageComponent } from './error-page/error-page.component'
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ShareModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    HomeComponent,
-    PostsComponent,
-    PostComponent,
-    AboutExtraComponent,
-    ErrorPageComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomePageComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, ShareModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { ColorDirective } from "./color.directive";
+import { PageNamePipe } from "./page-name.pipe";
+// import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations: [
+    ColorDirective,
+    PageNamePipe
+  ],
+  exports: [
+    ColorDirective,
+    PageNamePipe,
+    // CommonModule
+  ],
+
+})
+export class ShareModule {
+
+}
