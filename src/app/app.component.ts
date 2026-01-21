593f357466f6cb184dde9813dca26b4abf348340
SSerg-dev <srmail@mail.ru>
2026-01-21 18:26:30 -0500

    feat(finish angular-basic project) finish angular-basic project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { state, style, trigger } from '@angular/animations';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,23 +12,43 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    trigger('box' , [
-      state('start', style({
-        background: 'yellow'
-      })),
-      state('end', style({
-        background: 'salmon',
-        tranform: 'scale(1.2)'
-      }))
-    ])
-  ]
+    trigger('box', [
+      state(
+        'start',
+        style({
+          background: 'yellow',
+        })
+      ),
+      state(
+        'end',
+        style({
+          background: 'salmon',
+          transform: 'scale(1.2)',
+        })
+      ),
+      state(
+        'special',
+        style({
+          background: 'orange',
+          transform: 'scale(0.5)',
+          borderRadius: '50%',
+        })
+      ),
+
+      transition('start => end', animate(500)),
+      transition('end => start', animate('500ms ease-in-out')),
+      transition('start => special', animate('500ms')),
+    ]),
+  ],
 })
 export class AppComponent {
-  boxState = 'start'
+  boxState = 'start';
 
-// methods
-animate() {
-  this.boxState = this.boxState === 'start' ? 'end' : 'start'
-}
-
+  // methods
+  setSpecialState() {
+    this.boxState = 'special';
+  }
+  animate() {
+    this.boxState = this.boxState === 'start' ? 'end' : 'start';
+  }
 }
